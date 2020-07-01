5a39850f3116be8b2024a8f1edf0fb64c3c91e64
weffelms <stacie.weffelmeyer@wwt.com>
2020-07-01 10:09:43 -0500

    Section 2 challenge 4

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -116,3 +116,72 @@ function tipCalculator(billTotal) {
 
   return tip
 }
+
+/*****************************
+* CODING CHALLENGE 4
+*/
+
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+GOOD LUCK 😀
+*/
+
+const john = {
+  name: 'John Smith',
+  mass: 110,
+  height: 1.95,
+  calcBMI: function() {
+      this.bmi = this.mass / (this.height * this.height);
+      return this.bmi;
+  }
+}
+
+const mark = {
+  name: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function() {
+      this.bmi = this.mass / (this.height * this.height);
+      return this.bmi;
+  }
+}
+
+// if (john.calcBMI() > mark.calcBMI()) {
+//   console.log(john.fullName + ' has a higher BMI of ' + john.bmi);
+// } else if (mark.bmi > john.bmi) {
+//   console.log(mark.fullName + ' has a higher BMI of ' + mark.bmi);
+// } else {
+//   console.log('They have the same BMI');
+// }
+
+// const mark = {
+//   name: 'Mark',
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function() {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+// }
+// }
+
+// const john = {
+//   name: 'john',
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function() {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+// }
+// }
+
+const johnHigherBMI = mark.bmi < john.bmi
+john.calcBMI()
+mark.calcBMI()
+console.log('Is Mark\'s BMI higher than John\'s -' + johnHigherBMI)
+console.log(john.name, john.bmi, mark.name, mark.bmi)
