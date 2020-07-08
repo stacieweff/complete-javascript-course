5953bb6d80ada019ae594296bfd8498a8cc4b91c
weffelms <stacie.weffelmeyer@wwt.com>
2020-07-08 10:42:00 -0500

    section 4.49 - events handling rolling the dice

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,12 +8,27 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-let scores, roundScore, activePlayer, dice
+let scores, roundScore, activePlayer
 
 scores = [0, 0]
 roundScore = 0
 activePlayer = 0
 
-dice = Math.floor(Math.random() *6) +1
+// dice = Math.floor(Math.random() *6) +1
+// document.querySelector('#current-' + activePlayer).textContent = dice
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('currrent-1').textContent = '0'
 
-document.querySelector('#current-' + activePlayer).textContent = dice
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. Random Number
+  const dice = Math.floor(Math.random() *6) +1
+
+  // 2. Display the result
+  let diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block'
+  diceDOM.src = 'dice-' + dice + '.png'
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+})
